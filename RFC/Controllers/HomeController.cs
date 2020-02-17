4de2f862bd6eb26108b074125234a9cfba11fdc5
AdamFixter <56538759+AdamFixter@users.noreply.github.com>
2020-02-17 10:55:35 +0000

    Remove email from the navbar

--- RFC/Controllers/HomeController.cs
+++ RFC/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace RFC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
