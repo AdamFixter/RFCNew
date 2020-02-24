7311cde27d4e3213f295a8a82caf565f4f23b35f
AdamFixter <56538759+AdamFixter@users.noreply.github.com>
2020-02-24 16:19:07 +0000

    Added dropdown list.

--- RFC/Controllers/CreateNewController.cs
+++ RFC/Controllers/CreateNewController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -20,124 +21,180 @@ namespace RFC.Controllers
             _context = context;
         }
 
-        // GET: CreateNew
-        [Route("submissions")]
-        public async Task<IActionResult> Index([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, string searchString, string columnSelect, int? pageNumber, DateTime? DateTo)
+        private List<SelectListItem> FilteringChoices = new List<SelectListItem>()
         {
+            new SelectListItem { Value = "Priority", Text = "Priority" },
+            new SelectListItem { Value = "Product", Text = "Product" },
+            new SelectListItem { Value = "Customer", Text = "Customer" },
+            new SelectListItem { Value = "DueDate", Text = "Due Date" }
+        };
 
-            ViewData["CurrentSort"] = sortOrder;
+        [Route("submissions")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.columnSelect = FilteringChoices;   // Adds the pre-created list into the variable that will use it as dropdown values
 
-            if (searchString != null)
-            {
-                pageNumber = 1;
-            }
-            else
-            {
-                searchString = "";
-            }
-
-
-            List<SelectListItem> items = new List<SelectListItem>   //// Creates list of possible columns to select in drop-down menu
-            {
-                new SelectListItem { Value = "ID", Text = "ID" },
-                new SelectListItem { Value = "RFCType", Text = "RFC Type" },
-                new SelectListItem { Value = "ProductName", Text = "Product Name" },
-                new SelectListItem { Value = "CustomerName", Text = "Customer Name" },
-                new SelectListItem { Value = "RequestedDueDate", Text = "Requested Due Date" },
-            };
-            ViewBag.columnSelect = items;   //// Adds the pre-created list into the variable that will use it as dropdown values
-
-
-            ///// ViewBag.[something]Parm is used for sorting order by clicking column
-            ViewBag.IDSortParm = String.IsNullOrEmpty(sortOrder) ? "IDDesc" : "";
-            ViewBag.RFCTypeSortParm = sortOrder == "RFCTypeAsc" ? "RFCTypeDesc" : "RFCTypeAsc";
-            ViewBag.ProductNameSortParm = sortOrder == "ProductNameAsc" ? "ProductNameDesc" : "ProductNameAsc";
-            ViewBag.CustomerNameSortParm = sortOrder == "CustomerNameAsc" ? "CustomerNameDesc" : "CustomerNameAsc";
-            ViewBag.RequestedDueDateSortParm = sortOrder == "RequestedDueDateAsc" ? "RequestedDueDateDesc" : "RequestedDueDateAsc";
-
-            //// The 'submissions' is the variable with the data from the table
-            var submissions = from s in _context.CreateNew
-                              select s;
-
-            if (!String.IsNullOrEmpty(searchString))    //// Filters the 'submissions' data out depending on the search performed and column selected from the dropdown list
-            {
-                searchString = searchString.ToLower();
-                switch (columnSelect)
-                {
-                    case "ID":
-                        submissions = submissions.Where(s => s.ID.ToString().Contains(searchString));
-                        break;
-                    case "RequestedDueDate":
-                        submissions = submissions.Where(s => s.DueDate >= Convert.ToDateTime(searchString) && s.DueDate <= DateTo);
-                        break;
-                    //case "RFCType":
-                    //    if (Enum.GetNames(typeof(Priority)).ToList().IndexOf(searchString) != -1) //Check if the string includes a valid enum.
-                    //    {
-                    //        Priority foundPriority = (Priority)Enum.Parse(typeof(Priority), searchString); //Parse the search string to the Priority.
-                    //        submissions = submissions.Where(s => s.Priority == foundPriority);
-                    //    }
-                    //    else
-                    //    {
-                    //        submissions = Enumerable.Empty<CreateNew>().AsQueryable();
-                    //    }
-                    //    break;
-                    //case "ProductName":
-                    //    if (Enum.GetNames(typeof(Product)).ToList().IndexOf(searchString) != -1)
-                    //    {
-                    //        Product foundProduct = (Product)Enum.Parse(typeof(Product), searchString);
-                    //        submissions = submissions.Where(s => s.Product == foundProduct);
-                    //    }
-                    //    else
-                    //    {
-                    //        submissions = Enumerable.Empty<CreateNew>().AsQueryable();
-                    //    }
-                    //    break;
-                    case "CustomerName":
-                        submissions = submissions.Where(s => s.customers.Contains(searchString));
-                        break;
-                    default:
-                        break;
-
-                }
-            }
-
-
-            switch (sortOrder)  //// Sorts the columns when you click on them  by Asc or Desc
-            {
-                case "IDDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.ID);
-                    break;
-                case "RFCTypeAsc":
-                    submissions = submissions.OrderBy(submission => submission.Priority);
-                    break;
-                case "RFCTypeDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.Priority);
-                    break;
-                case "ProductNameAsc":
-                    submissions = submissions.OrderBy(submission => submission.Product);
-                    break;
-                case "ProductNameDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.Product);
-                    break;
-                case "CustomerNameAsc":
-                    submissions = submissions.OrderBy(submission => submission.customers);
-                    break;
-                case "CustomerNameDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.customers);
-                    break;
-                case "RequestedDueDateAsc":
-                    submissions = submissions.OrderBy(submission => submission.DueDate);
-                    break;
-                case "RequestedDueDateDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.DueDate);
-                    break;
-                default:
-                    submissions = submissions.OrderBy(submission => submission.ID);
-                    break;
-            }
-            int pageSize = 5;
-            return View(await PaginatedList<CreateNew>.CreateAsync(submissions.AsNoTracking(), pageNumber ?? 1, pageSize));
+            CreateNewViewModel vm = new CreateNewViewModel();
+            vm.PossibleFilters = FilteringChoices;
+            return View(vm);
         }
+
+        [HttpPost]
+        public ActionResult getDropdownFor(string name)
+        {
+            switch (name)
+            {
+                case "Priority":
+                    return Json(new SelectList(getAllPriorities(), "Value", "Text"));
+                case "Product":
+                    return Json(new SelectList(getAllProducts(), "Value", "Text"));
+                case "Customer":
+                    return Json(new SelectList(getAllCustomers(), "Value", "Text"));
+                default:
+                    break;
+            }
+            return NotFound();
+        }
+
+        public List<SelectListItem> getAllPriorities()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            Enum.GetValues(typeof(Priority)).Cast<Priority>().ToList().ForEach(priority => items.Add(new SelectListItem { Value = priority.ToString(), Text = priority.ToString() }));
+            return items;
+        }
+        public List<SelectListItem> getAllProducts()
+        {
+            List <SelectListItem> items = new List<SelectListItem>();
+            Enum.GetValues(typeof(Product)).Cast<Product>().ToList().ForEach(product => items.Add(new SelectListItem { Value = product.ToString(), Text = product.ToString() }));
+            return items;
+        }
+        public List<SelectListItem> getAllCustomers()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            Enum.GetValues(typeof(CustomersAffected)).Cast<CustomersAffected>().ToList().ForEach(customer => items.Add(new SelectListItem { Value = customer.ToString(), Text = customer.ToString() }));
+            return items;
+        }
+        // GET: CreateNew
+        //[Route("submissions")]
+        //public async Task<IActionResult> Index([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, string searchString, string columnSelect, int? pageNumber, DateTime? DateTo)
+        //{
+
+        //    ViewData["CurrentSort"] = sortOrder;
+
+        //    if (searchString != null)
+        //    {
+        //        pageNumber = 1;
+        //    }
+        //    else
+        //    {
+        //        searchString = "";
+        //    }
+
+
+        //    List<SelectListItem> items = new List<SelectListItem>   //// Creates list of possible columns to select in drop-down menu
+        //    {
+        //        new SelectListItem { Value = "ID", Text = "ID" },
+        //        new SelectListItem { Value = "RFCType", Text = "RFC Type" },
+        //        new SelectListItem { Value = "ProductName", Text = "Product Name" },
+        //        new SelectListItem { Value = "CustomerName", Text = "Customer Name" },
+        //        new SelectListItem { Value = "RequestedDueDate", Text = "Requested Due Date" },
+        //    };
+        //    ViewBag.columnSelect = items;   //// Adds the pre-created list into the variable that will use it as dropdown values
+
+
+        //    ///// ViewBag.[something]Parm is used for sorting order by clicking column
+        //    ViewBag.IDSortParm = String.IsNullOrEmpty(sortOrder) ? "IDDesc" : "";
+        //    ViewBag.RFCTypeSortParm = sortOrder == "RFCTypeAsc" ? "RFCTypeDesc" : "RFCTypeAsc";
+        //    ViewBag.ProductNameSortParm = sortOrder == "ProductNameAsc" ? "ProductNameDesc" : "ProductNameAsc";
+        //    ViewBag.CustomerNameSortParm = sortOrder == "CustomerNameAsc" ? "CustomerNameDesc" : "CustomerNameAsc";
+        //    ViewBag.RequestedDueDateSortParm = sortOrder == "RequestedDueDateAsc" ? "RequestedDueDateDesc" : "RequestedDueDateAsc";
+
+        //    //// The 'submissions' is the variable with the data from the table
+        //    var submissions = from s in _context.CreateNew
+        //                      select s;
+
+        //    if (!String.IsNullOrEmpty(searchString))    //// Filters the 'submissions' data out depending on the search performed and column selected from the dropdown list
+        //    {
+        //        searchString = searchString.ToLower();
+        //        switch (columnSelect)
+        //        {
+        //            case "RequestedDueDate":
+        //                submissions = submissions.Where(s => s.DueDate >= Convert.ToDateTime(searchString) && s.DueDate <= DateTo);
+        //                break;
+        //            //case "RFCType":
+        //            //    if (Enum.GetNames(typeof(Priority)).ToList().IndexOf(searchString) != -1) //Check if the string includes a valid enum.
+        //            //    {
+        //            //        Priority foundPriority = (Priority)Enum.Parse(typeof(Priority), searchString); //Parse the search string to the Priority.
+        //            //        submissions = submissions.Where(s => s.Priority == foundPriority);
+        //            //    }
+        //            //    else
+        //            //    {
+        //            //        submissions = Enumerable.Empty<CreateNew>().AsQueryable();
+        //            //    }
+        //            //    break;
+        //            //case "ProductName":
+        //            //    if (Enum.GetNames(typeof(Product)).ToList().IndexOf(searchString) != -1)
+        //            //    {
+        //            //        Product foundProduct = (Product)Enum.Parse(typeof(Product), searchString);
+        //            //        submissions = submissions.Where(s => s.Product == foundProduct);
+        //            //    }
+        //            //    else
+        //            //    {
+        //            //        submissions = Enumerable.Empty<CreateNew>().AsQueryable();
+        //            //    }
+        //            //    break;
+        //            case "CustomerName":
+        //                submissions = submissions.Where(s => s.customers.Contains(searchString));
+        //                Enum.GetNames(typeof(CustomersAffected)).ToList().ForEach(customer => items.Add(new SelectListItem() { Value = customer, Text = customer }));
+        //                break;
+        //            default:
+        //                break;
+
+        //        }
+        //    }
+
+
+        //    switch (sortOrder)  //// Sorts the columns when you click on them  by Asc or Desc
+        //    {
+        //        case "IDDesc":
+        //            submissions = submissions.OrderByDescending(submission => submission.ID);
+        //            break;
+        //        case "RFCTypeAsc":
+        //            submissions = submissions.OrderBy(submission => submission.Priority);
+        //            break;
+        //        case "RFCTypeDesc":
+        //            submissions = submissions.OrderByDescending(submission => submission.Priority);
+        //            break;
+        //        case "ProductNameAsc":
+        //            submissions = submissions.OrderBy(submission => submission.Product);
+        //            break;
+        //        case "ProductNameDesc":
+        //            submissions = submissions.OrderByDescending(submission => submission.Product);
+        //            break;
+        //        case "CustomerNameAsc":
+        //            submissions = submissions.OrderBy(submission => submission.customers);
+        //            break;
+        //        case "CustomerNameDesc":
+        //            submissions = submissions.OrderByDescending(submission => submission.customers);
+        //            break;
+        //        case "RequestedDueDateAsc":
+        //            submissions = submissions.OrderBy(submission => submission.DueDate);
+        //            break;
+        //        case "RequestedDueDateDesc":
+        //            submissions = submissions.OrderByDescending(submission => submission.DueDate);
+        //            break;
+        //        default:
+        //            submissions = submissions.OrderBy(submission => submission.ID);
+        //            break;
+        //    }
+        //    int pageSize = 5;
+        //    CreateNewViewModel vm = new CreateNewViewModel() {
+        //        Requests = await PaginatedList<CreateNew>.CreateAsync(submissions.AsNoTracking(), pageNumber ?? 1, pageSize),
+        //        ListItems = items
+        //    };
+
+        //    return View(vm);
+        //}
 
         // GET: CreateNew/Details/5
         public async Task<IActionResult> Details([Bind("ID,Name,Role,DomainUser")] User CurrentUser, long? id)
