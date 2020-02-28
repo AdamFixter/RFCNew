f319ae6e05afb712e18bf84a21dffbd658b885cf
AdamFixter <56538759+AdamFixter@users.noreply.github.com>
2020-02-28 10:36:48 +0000

    Updated from Master

--- RFC/Controllers/CreateNewController.cs
+++ RFC/Controllers/CreateNewController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,83 +24,15 @@ namespace RFC.Controllers
 
         // GET: CreateNew
         [Route("submissions")]
-        public async Task<IActionResult> Index([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, string searchString, string columnSelect, int? pageNumber, DateTime? DateTo)
+        public async Task<IActionResult> Index([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, string searchID, Priority searchPriority, Product searchProduct, string searchCustomer, string searchStatus, DateTime? searchDateStart, DateTime? searchDateEnd, string columnSelect, int? pageNumber)
         {
-            var varOne = 0;
-            var varTwo = 1;
-            var tuple = (myVar: varOne, myVar2: varTwo);
+            Debug.WriteLine($"\n\n{sortOrder}\n{searchID}\n{searchPriority.ToString()}\n{searchProduct.ToString()}\n{searchCustomer}\n{searchStatus}");
             ViewData["CurrentSort"] = sortOrder;
 
-            if (searchString != null)
-            {
-                pageNumber = 1;
-            }
-            else
-            {
-                searchString = "";
-            }
-
-
-            List<SelectListItem> items = new List<SelectListItem>   //// Creates list of possible columns to select in drop-down menu
-            {
-                new SelectListItem { Value = "ID", Text = "ID" },
-                new SelectListItem { Value = "RFCType", Text = "Type" },
-                new SelectListItem { Value = "ProductName", Text = "Product Name" },
-                new SelectListItem { Value = "CustomerName", Text = "Customer Name" },
-                new SelectListItem { Value = "RequestedDueDate", Text = "Requested Due Date" },
-                new SelectListItem { Value = "Approved", Text = "Approved" }
-            };
-            ViewBag.columnSelect = items;   //// Adds the pre-created list into the variable that will use it as dropdown values
-
-
-            ///// ViewBag.[something]Parm is used for sorting order by clicking column
-            ViewBag.IDSortParm = String.IsNullOrEmpty(sortOrder) ? "IDDesc" : "";
-            ViewBag.RFCTypeSortParm = sortOrder == "RFCTypeAsc" ? "RFCTypeDesc" : "RFCTypeAsc";
-            ViewBag.ProductNameSortParm = sortOrder == "ProductNameAsc" ? "ProductNameDesc" : "ProductNameAsc";
-            ViewBag.CustomerNameSortParm = sortOrder == "CustomerNameAsc" ? "CustomerNameDesc" : "CustomerNameAsc";
-            ViewBag.RequestedDueDateSortParm = sortOrder == "RequestedDueDateAsc" ? "RequestedDueDateDesc" : "RequestedDueDateAsc";
-            ViewBag.Approved = sortOrder == "ApprovedAsc" ? "ApprovedDesc" : "ApprovedAsc";
-
-            //// The 'submissions' is the variable with the data from the table
             var submissions = from s in _context.CreateNew
                               select s;
 
-            if (!String.IsNullOrEmpty(searchString))    //// Filters the 'submissions' data out depending on the search performed and column selected from the dropdown list
-            {
-                searchString = searchString.ToLower();
-                switch (columnSelect)
-                {
-                    case "ID":
-                        submissions = submissions.Where(s => s.ID.ToString().Contains(searchString));
-                        break;
-                    case "RequestedDueDate":
-                        submissions = submissions.Where(s => s.DueDate >= Convert.ToDateTime(searchString) && s.DueDate <= DateTo);
-                        break;
-                    case "RFCType":
-                        searchString = textInfo.ToTitleCase(searchString);
-                        if (Enum.GetNames(typeof(Priority)).ToList().IndexOf(searchString) != -1) //Check if the string includes a valid enum.
-                        {
-                            Debug.WriteLine("\n\n   - Found: Yes");
-                            Priority foundPriority = (Priority)Enum.Parse(typeof(Priority), searchString); //Parse the search string to the Priority.
-                            submissions = submissions.Where(s => s.Priority == foundPriority);
-                            Debug.WriteLine($"\n\n   - Filtered: {submissions}");
-                        }
-                        break;
-                    case "ProductName":
-                        searchString = textInfo.ToTitleCase(searchString);
-                        if (Enum.GetNames(typeof(Product)).ToList().IndexOf(searchString) != -1)
-                        {
-                            Product foundProduct = (Product)Enum.Parse(typeof(Product), searchString);
-                            submissions = submissions.Where(s => s.Product == foundProduct);
-                        }
-                        break;
-                    case "CustomerName":
-                        submissions = submissions.Where(s => s.customers.Contains(searchString));
-                        break;
-                }
-            }
 
-            ViewBag.sortOrder = sortOrder;
             switch (sortOrder)  //// Sorts the columns when you click on them  by Asc or Desc
             {
                 case "IDDesc":
@@ -141,7 +74,10 @@ namespace RFC.Controllers
             }
             int pageSize = 5;
             return View(await PaginatedList<CreateNew>.CreateAsync(submissions.AsNoTracking(), pageNumber ?? 1, pageSize));
+
+
         }
+
 
         // GET: CreateNew/Details/5
         public async Task<IActionResult> Details([Bind("ID,Name,Role,DomainUser")] User CurrentUser, long? id)
@@ -185,7 +121,8 @@ namespace RFC.Controllers
             {
                 _context.Add(createNew);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                TempData["submittedID"] = createNew.ID.ToString();
+                return RedirectToAction("Index", "Home", new { area = "" });
             }
             return View(createNew);
         }
@@ -203,20 +140,25 @@ namespace RFC.Controllers
             {
                 return NotFound();
             }
+            TempData["lastRowID"] = id.ToString();
             if (ModelState.IsValid)
             {
                 createNew.Approved = !createNew.Approved;
                 _context.Update(createNew);
                 await _context.SaveChangesAsync();
+                TempData["stateMessage"] = createNew.Approved;
+                TempData["stateStatus"] = true;
                 return RedirectToAction(nameof(Index));
             }
+            TempData["stateMessage"] = !createNew.Approved;
+            TempData["stateStatus"] = false;
             return RedirectToAction(nameof(Index));
         }
 
         // GET: CreateNew/Delete/5
         public async Task<IActionResult> Delete([Bind("ID,Name,Role,DomainUser")] User CurrentUser, long? id)
         {
-            if (CurrentUser.Role != UserRole.Power) return RedirectToAction("Index", "Home", new { area = "" });
+            //if (CurrentUser.Role == UserRole.Standard) return RedirectToAction("Index", "Home", new { area = "" });   //// The CurrentUser.Role isn't valid for this to work
 
             if (id == null)
             {
@@ -238,8 +180,7 @@ namespace RFC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed([Bind("ID,Name,Role,DomainUser")] User CurrentUser,long id)
         {
-            if (CurrentUser.Role != UserRole.Power) return RedirectToAction("Index", "Home", new { area = "" });
-
+            //if (CurrentUser.Role == UserRole.Standard) return RedirectToAction("Index", "Home", new { area = "" });   //// The CurrentUser.Role isn't valid for this to work
             var createNew = await _context.CreateNew.FindAsync(id);
             _context.CreateNew.Remove(createNew);
             await _context.SaveChangesAsync();
@@ -249,85 +190,6 @@ namespace RFC.Controllers
         private bool CreateNewExists(long id)
         {
             return _context.CreateNew.Any(e => e.ID == id);
-        }
-
-
-        [Route("WeeklyAgenda")]
-        public async Task<IActionResult> WeeklyAgenda([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, DateTime? DateFrom, string columnSelect, int? pageNumber, DateTime? DateTo)
-        {
-            ViewData["CurrentSort"] = sortOrder;
-
-            List<SelectListItem> items = new List<SelectListItem>   //// Creates list of possible columns to select in drop-down menu
-            {
-                new SelectListItem { Value = "ID", Text = "ID" },
-                new SelectListItem { Value = "RFCType", Text = "Type" },
-                new SelectListItem { Value = "ProductName", Text = "Product Name" },
-                new SelectListItem { Value = "CustomerName", Text = "Customer Name" },
-                new SelectListItem { Value = "RequestedDueDate", Text = "Requested Due Date" },
-                new SelectListItem { Value = "Approved", Text = "Approved" }
-            };
-            ViewBag.columnSelect = items;   //// Adds the pre-created list into the variable that will use it as dropdown values
-
-
-            ///// ViewBag.[something]Parm is used for sorting order by clicking column
-            ViewBag.IDSortParm = String.IsNullOrEmpty(sortOrder) ? "IDDesc" : "";
-            ViewBag.RFCTypeSortParm = sortOrder == "RFCTypeAsc" ? "RFCTypeDesc" : "RFCTypeAsc";
-            ViewBag.ProductNameSortParm = sortOrder == "ProductNameAsc" ? "ProductNameDesc" : "ProductNameAsc";
-            ViewBag.CustomerNameSortParm = sortOrder == "CustomerNameAsc" ? "CustomerNameDesc" : "CustomerNameAsc";
-            ViewBag.RequestedDueDateSortParm = sortOrder == "RequestedDueDateAsc" ? "RequestedDueDateDesc" : "RequestedDueDateAsc";
-            ViewBag.Approved = sortOrder == "ApprovedAsc" ? "ApprovedDesc" : "ApprovedAsc";
-
-            //// The 'submissions' is the variable with the data from the table
-            var submissions = from s in _context.CreateNew
-                              select s;
-
-            if (!String.IsNullOrEmpty("DateTo"))    //// Filters the 'submissions' data out depending on the search performed and column selected from the dropdown list
-            {
-                 submissions = submissions.Where(s => s.DueDate >= DateFrom && s.DueDate <= DateTo);                
-            }
-
-            ViewBag.sortOrder = sortOrder;
-            switch (sortOrder)  //// Sorts the columns when you click on them  by Asc or Desc
-            {
-                case "IDDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.ID);
-                    break;
-                case "RFCTypeAsc":
-                    submissions = submissions.OrderBy(submission => submission.Priority);
-                    break;
-                case "RFCTypeDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.Priority);
-                    break;
-                case "ProductNameAsc":
-                    submissions = submissions.OrderBy(submission => submission.Product);
-                    break;
-                case "ProductNameDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.Product);
-                    break;
-                case "CustomerNameAsc":
-                    submissions = submissions.OrderBy(submission => submission.customers);
-                    break;
-                case "CustomerNameDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.customers);
-                    break;
-                case "RequestedDueDateAsc":
-                    submissions = submissions.OrderBy(submission => submission.DueDate);
-                    break;
-                case "RequestedDueDateDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.DueDate);
-                    break;
-                case "ApprovedAsc":
-                    submissions = submissions.OrderBy(submission => submission.Approved);
-                    break;
-                case "ApprovedDesc":
-                    submissions = submissions.OrderByDescending(submission => submission.Approved);
-                    break;
-                default:
-                    submissions = submissions.OrderBy(submission => submission.ID);
-                    break;
-            }
-            int pageSize = 5;
-            return View(await PaginatedList<CreateNew>.CreateAsync(submissions.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
     }
 }
