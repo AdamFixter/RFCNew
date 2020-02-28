6aace6703605aa574ef601c7264396c0c493eb7d
AdamFixter <56538759+AdamFixter@users.noreply.github.com>
2020-02-28 08:57:55 +0000

    Revert "Merge branch 'master' into AdamF"
    This reverts commit 402dc5d3eba51bc4192d74ba5f213671b42d6caf, reversing
    changes made to 021de1e145316da7d7f6e2569a25abdf9cd974ad.

--- RFC/Controllers/CreateNewController.cs
+++ RFC/Controllers/CreateNewController.cs
@@ -250,5 +250,84 @@ namespace RFC.Controllers
         {
             return _context.CreateNew.Any(e => e.ID == id);
         }
+
+
+        [Route("WeeklyAgenda")]
+        public async Task<IActionResult> WeeklyAgenda([Bind("ID,Name,Role,DomainUser")] User CurrentUser, string sortOrder, DateTime? DateFrom, string columnSelect, int? pageNumber, DateTime? DateTo)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+
+            List<SelectListItem> items = new List<SelectListItem>   //// Creates list of possible columns to select in drop-down menu
+            {
+                new SelectListItem { Value = "ID", Text = "ID" },
+                new SelectListItem { Value = "RFCType", Text = "Type" },
+                new SelectListItem { Value = "ProductName", Text = "Product Name" },
+                new SelectListItem { Value = "CustomerName", Text = "Customer Name" },
+                new SelectListItem { Value = "RequestedDueDate", Text = "Requested Due Date" },
+                new SelectListItem { Value = "Approved", Text = "Approved" }
+            };
+            ViewBag.columnSelect = items;   //// Adds the pre-created list into the variable that will use it as dropdown values
+
+
+            ///// ViewBag.[something]Parm is used for sorting order by clicking column
+            ViewBag.IDSortParm = String.IsNullOrEmpty(sortOrder) ? "IDDesc" : "";
+            ViewBag.RFCTypeSortParm = sortOrder == "RFCTypeAsc" ? "RFCTypeDesc" : "RFCTypeAsc";
+            ViewBag.ProductNameSortParm = sortOrder == "ProductNameAsc" ? "ProductNameDesc" : "ProductNameAsc";
+            ViewBag.CustomerNameSortParm = sortOrder == "CustomerNameAsc" ? "CustomerNameDesc" : "CustomerNameAsc";
+            ViewBag.RequestedDueDateSortParm = sortOrder == "RequestedDueDateAsc" ? "RequestedDueDateDesc" : "RequestedDueDateAsc";
+            ViewBag.Approved = sortOrder == "ApprovedAsc" ? "ApprovedDesc" : "ApprovedAsc";
+
+            //// The 'submissions' is the variable with the data from the table
+            var submissions = from s in _context.CreateNew
+                              select s;
+
+            if (!String.IsNullOrEmpty("DateTo"))    //// Filters the 'submissions' data out depending on the search performed and column selected from the dropdown list
+            {
+                 submissions = submissions.Where(s => s.DueDate >= DateFrom && s.DueDate <= DateTo);                
+            }
+
+            ViewBag.sortOrder = sortOrder;
+            switch (sortOrder)  //// Sorts the columns when you click on them  by Asc or Desc
+            {
+                case "IDDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.ID);
+                    break;
+                case "RFCTypeAsc":
+                    submissions = submissions.OrderBy(submission => submission.Priority);
+                    break;
+                case "RFCTypeDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.Priority);
+                    break;
+                case "ProductNameAsc":
+                    submissions = submissions.OrderBy(submission => submission.Product);
+                    break;
+                case "ProductNameDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.Product);
+                    break;
+                case "CustomerNameAsc":
+                    submissions = submissions.OrderBy(submission => submission.customers);
+                    break;
+                case "CustomerNameDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.customers);
+                    break;
+                case "RequestedDueDateAsc":
+                    submissions = submissions.OrderBy(submission => submission.DueDate);
+                    break;
+                case "RequestedDueDateDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.DueDate);
+                    break;
+                case "ApprovedAsc":
+                    submissions = submissions.OrderBy(submission => submission.Approved);
+                    break;
+                case "ApprovedDesc":
+                    submissions = submissions.OrderByDescending(submission => submission.Approved);
+                    break;
+                default:
+                    submissions = submissions.OrderBy(submission => submission.ID);
+                    break;
+            }
+            int pageSize = 5;
+            return View(await PaginatedList<CreateNew>.CreateAsync(submissions.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
     }
 }
